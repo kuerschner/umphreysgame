06af3e1d64b59093ce0a30e67204b0316f2b9398
Mike <mike.kuerschner@gmail.com>
2020-02-04 12:23:08 -0700

    adding in formik to handle board data

--- src/components/SongInput.js
+++ src/components/SongInput.js
@@ -2,14 +2,23 @@ import React from 'react';
 import { TextField } from '@material-ui/core';
 
 const SongInput = props => {
+    let value;
+
+    props.type === 'setlist' ? value = props.value[props.points] : value = props.value[props.name];
+
     return (
         <div className='row full-width middle-xs center-xs'>
             <div className='col-xs-2 end-xs'>
                 <p>{props.type === 'setlist' ? `${props.points}. ` : `${props.points}: `}</p>
-
             </div>
             <div className='col-xs-10 start-xs'>
-                <TextField label={props.explaination ? props.explaination : ''} style={{width: '80%'}} />
+                <TextField 
+                    label={props.explanation ? props.explanation : ''} 
+                    style={{width: '80%'}} 
+                    value={value} 
+                    name={props.type === 'additional' ? props.name : props.points}
+                    onChange={props.onChange}
+                />
             </div>
         </div>
     )
--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,8 +1,44 @@
 import React from 'react'
 import SongInput from '../components/SongInput'
 import { Button } from '@material-ui/core'
+import { useFormik } from 'formik';
 
 const Board = (props) => {
+    const formik = useFormik({
+        initialValues: {
+            15: '',
+            14: '',
+            13: '',
+            12: '',
+            11: '',
+            10: '',
+            9: '',
+            8: '',
+            7: '',
+            6: '',
+            5: '',
+            4: '',
+            3: '',
+            2: '',
+            1: '',
+            cover1: '',
+            cover2: '',
+            cover3: '',
+            artist1: '',
+            artist2: '',
+            artist3: '',
+            opener1: '',
+            opener2: '',
+            opener3: '',
+            instrumental1: '',
+            instrumental2: '',
+            instrumental3: '',
+            wildcard: '',
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
     return (
         <>
             <div className='row center-xs middle-xs full-width'>
@@ -17,21 +53,96 @@ const Board = (props) => {
                     <p>Number of points correspond with number associated on the line. The goal is to get the most points.</p>
                 </div>
                 <div className='col-xs-12'>
-                    <SongInput points='15' type='setlist'/>
-                    <SongInput points='14' type='setlist'/>
-                    <SongInput points='13' type='setlist'/>
-                    <SongInput points='12' type='setlist'/>
-                    <SongInput points='11' type='setlist'/>
-                    <SongInput points='10' type='setlist'/>
-                    <SongInput points='9' type='setlist'/>
-                    <SongInput points='8' type='setlist'/>
-                    <SongInput points='7' type='setlist'/>
-                    <SongInput points='6' type='setlist'/>
-                    <SongInput points='5' type='setlist'/>
-                    <SongInput points='4' type='setlist'/>
-                    <SongInput points='3' type='setlist'/>
-                    <SongInput points='2' type='setlist'/>
-                    <SongInput points='1' type='setlist'/>
+                    <SongInput 
+                        points='15' 
+                        type='setlist'
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
+                    <SongInput 
+                        points='14' 
+                        type='setlist'
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
+                    <SongInput 
+                        points='13' 
+                        type='setlist'
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
+                    <SongInput 
+                        points='12' 
+                        type='setlist'
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
+                    <SongInput 
+                        points='11' 
+                        type='setlist'
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
+                    <SongInput 
+                        points='10' 
+                        type='setlist'
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
+                    <SongInput 
+                        points='9' 
+                        type='setlist'
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
+                    <SongInput 
+                        points='8' 
+                        type='setlist'
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
+                    <SongInput 
+                        points='7' 
+                        type='setlist'
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
+                    <SongInput 
+                        points='6' 
+                        type='setlist'
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
+                    <SongInput 
+                        points='5' 
+                        type='setlist'
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
+                    <SongInput 
+                        points='4' 
+                        type='setlist'
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
+                    <SongInput 
+                        points='3' 
+                        type='setlist'
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
+                    <SongInput 
+                        points='2' 
+                        type='setlist'
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
+                    <SongInput 
+                        points='1' 
+                        type='setlist'
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
                 </div>
             </div>
             <div className='row center-xs middle-xs full-width'>
@@ -39,48 +150,139 @@ const Board = (props) => {
                     <h2>Additional Points</h2>
                 </div>
                 <div className='col-xs-12'>
-                    <SongInput points='15' type='additional' explaination={`Cover (teases don't count)`}/>
+                    <SongInput 
+                        points='15' 
+                        type='additional' 
+                        name='cover1' 
+                        explanation={`Cover (teases don't count)`}
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
                 </div>
                 <div className='col-xs-12'>
-                    <SongInput points='15' type='additional' explaination={`Cover (teases don't count)`}/>
+                    <SongInput 
+                        points='15' 
+                        type='additional' 
+                        name='cover2' 
+                        explanation={`Cover (teases don't count)`}
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
                 </div>
                 <div className='col-xs-12'>
-                    <SongInput points='15' type='additional' explaination={`Cover (teases don't count)`}/>
+                    <SongInput 
+                        points='15' 
+                        type='additional' 
+                        name='cover3' 
+                        explanation={`Cover (teases don't count)`}
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
                 </div>
                 <div className='col-xs-12'>
-                    <SongInput points='10' type='additional' explaination={`Artist covered (original artist)`}/>
+                    <SongInput 
+                        points='10' 
+                        type='additional' 
+                        name='artist1' 
+                        explanation={`Artist covered (original artist)`}
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
                 </div>
                 <div className='col-xs-12'>
-                    <SongInput points='10' type='additional' explaination={`Artist covered (original artist)`}/>
+                    <SongInput 
+                        points='10' 
+                        type='additional' 
+                        name='artist2' 
+                        explanation={`Artist covered (original artist)`}
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
                 </div>
                 <div className='col-xs-12'>
-                    <SongInput points='10' type='additional' explaination={`Artist covered (original artist)`}/>
+                    <SongInput 
+                        points='10' 
+                        type='additional' 
+                        name='artist3' 
+                        explanation={`Artist covered (original artist)`}
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
                 </div>
                 <div className='col-xs-12'>
-                    <SongInput points='5' type='additional' explaination={`Openers (any night/any set)`}/>
+                    <SongInput 
+                        points='5' 
+                        type='additional' 
+                        name='opener1' 
+                        explanation={`Openers (any night/any set)`}
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
                 </div>
                 <div className='col-xs-12'>
-                    <SongInput points='5' type='additional' explaination={`Openers (any night/any set)`}/>
+                    <SongInput 
+                        points='5' 
+                        type='additional' 
+                        name='opener2' 
+                        explanation={`Openers (any night/any set)`}
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
                 </div>
                 <div className='col-xs-12'>
-                    <SongInput points='5' type='additional' explaination={`Openers (any night/any set)`}/>
+                    <SongInput 
+                        points='5' 
+                        type='additional' 
+                        name='opener3' 
+                        explanation={`Openers (any night/any set)`}
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
                 </div>
                 <div className='col-xs-12'>
-                    <SongInput points='5' type='additional' explaination={`Instrumental UM song`}/>
+                    <SongInput 
+                        points='5' 
+                        type='additional' 
+                        name='instrumental1' 
+                        explanation={`Instrumental UM song`}
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
                 </div>
                 <div className='col-xs-12'>
-                    <SongInput points='5' type='additional' explaination={`Instrumental UM song`}/>
+                    <SongInput 
+                        points='5' 
+                        type='additional' 
+                        name='instrumental2' 
+                        explanation={`Instrumental UM song`}
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
                 </div>
                 <div className='col-xs-12'>
-                    <SongInput points='5' type='additional' explaination={`Instrumental UM song`}/>
+                    <SongInput 
+                        points='5' 
+                        type='additional' 
+                        name='instrumental3' 
+                        explanation={`Instrumental UM song`}
+                        onChange={formik.handleChange}
+                        value={formik.values}
+                    />
                 </div>
                 <div className='col-xs-12'>
-                    <SongInput points='25' type='additional' explaination={`Wild Card (song that hasn’t been played in at least 50 shows- cover or Um original)`}/>
+                    <SongInput 
+                        points='25' 
+                        type='additional' 
+                        name='wildcard' 
+                        explanation={`Wild Card (song that hasn’t been played in at least 50 shows- cover or Um original)`}
+                                                onChange={formik.handleChange}
+                        value={formik.values}
+                    />
                 </div>
             </div>
             <div className='row center-xs middle-xs full-width'>
                 <div className='col-xs-12 top-spacer bottom-spacer'>
-                    <Button className='primary-button' style={{backgroundColor: 'var(--main-green)', padding: '1rem 2rem', color: '#fff'}}>Save My Picks</Button>
+                    <Button className='primary-button' style={{backgroundColor: 'var(--main-green)', padding: '1rem 2rem', color: '#fff'}} onClick={formik.onSubmit}>Save My Picks</Button>
                 </div>
             </div>
         </>
